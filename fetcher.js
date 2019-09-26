c64d59d1c64e3f61a38091c0438f311f2d3e8812
caitlining <caitlincing@gmail.com>
2019-09-26 02:23:07 +0000

    Completed assignment and submitting final function. Two new index files have been created which we used for testing my function. I will likely come back and refactor, as I should break up my function into smaller functions, but currently it works

--- fetcher.js
+++ fetcher.js
@@ -1,23 +1,47 @@
 const request = require('request');
 const fs = require('fs');
+const readline = require('readline');
 
 const commandLineArgArray = process.argv;
 const commandLineURL = commandLineArgArray[2];
 const commandLineLocalFilePath = commandLineArgArray[3];
 
-request(commandLineURL, (error, response, body) => {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    fs.writeFile(commandLineLocalFilePath, body, (err) => {
-      if (err) throw err;
-      const stats = fs.statSync(commandLineLocalFilePath);
-      const fileSizeInBytes = stats.size;
-      console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${commandLineLocalFilePath}`);
-    })
-  };
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-//console.log(commandLineURL, commandLineLocalFilePath);
-// console.log(commandLineURL);
+const downloadPage = function(inputURL, destinationFile) {
+  
+  fs.access(destinationFile, fs.F_OK, (err) => {
+    if (!err) {
+      rl.question('This file already exists.\nTo overwrite the file, type in Y and hit enter.\nIf you do not wish to overwrite the file, type in any other key and hit enter to quit the program\n', (answer) => {
+        if (answer === "Y") {
+          request(inputURL, (error, response, body) => {
+            if (error) {
+              console.log('The URL that you gave me was not legit, so I am quitting now, byeeee!');
+              process.exit;
+            } else {
+              fs.writeFile(destinationFile, body, (err) => {
+                if (err) throw err;
+                const stats = fs.statSync(destinationFile);
+                const fileSizeInBytes = stats.size;
+                console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${destinationFile}`);
+              });
+            }
+          });
+          rl.close();
+        } else {
+          rl.close();
+          process.exit;
+        }
+      });
+    } else {
+      console.log('The given destination file does not exist, so I am quitting');
+      process.exit();
+    }
+  });
+};
 
+
+downloadPage(commandLineURL, commandLineLocalFilePath);
